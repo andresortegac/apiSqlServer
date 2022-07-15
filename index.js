61ef95fae6038470b8eab830a55450b46da66937
Andres Ortega <andres.ortegac97@gmail.com>
2022-07-15 10:59:04 -0500

    se agregaron comentarios al index.js

--- index.js
+++ index.js
@@ -8,12 +8,16 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+
+//middlewares-------------
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
+//routes------------------
 app.use('/api', userRoutes.routes);
 
+//server running----------
 app.listen(config.port, () => {
     console.log('app listening on url http://localhost:' + config.port )
 });
